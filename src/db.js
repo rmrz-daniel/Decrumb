caf3dc7a46ba4ae22a46f7d7c8de9f25e3cd68f8
Daniel Ramirez <daniel.ramirez@edu.sait.ca>
2022-11-24 00:20:49 -0700

    Update to db
    Seasoned the user

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import Dexie from 'dexie';
 
 export const db = new Dexie('Subnets');
-db.version(12).stores({
+db.version(13).stores({
   subnet: '++id, collection, name, subnet, vlan, vrf, gw, location, state',
-  user: '++id, username, password'
+  user: '++id, username, password, salt'
 });
